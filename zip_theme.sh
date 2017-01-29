29f50c534a9265f69c6fe57c53e8968da9130ae6
Nate Kem <me@nateserk.com>
2017-01-28 22:45:46 -0800

    [ Shell Batch Scrtip ] Modified zip_theme.sh to exclude export folder and files in it as well as adding timestamp to a final zip file.

--- zip_theme.sh
+++ zip_theme.sh
@@ -1,5 +1,6 @@
 # Shell batch on Mac OSX for zip all theme's files.
 #!/bin/bash
-echo "Zipping 'nateserk_tinycup' theme.\n";
-sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/ -X "export/nateserk_tinycup.zip" *
+timestamp=$(date +%s)
+echo "Zipping 'nateserk_tinycup' theme generated at timestamp=$timestamp \n";
+sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/* -X "export/nateserk_tinycup_$timestamp.zip" *
 sudo rm ".DS_Store"
