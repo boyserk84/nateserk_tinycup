65ed1f4fc7d70825d80d30cef4ffb9a5f1fd733e
Nate Kem <me@nateserk.com>
2017-02-05 13:49:37 -0800

    [ Shell Script ] Remove timestamp from the file zip name.

--- zip_theme.sh
+++ zip_theme.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 timestamp=$(date +%s)
 echo "Zipping 'nateserk_tinycup' theme generated at timestamp=$timestamp \n";
-sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/* -X "export/nateserk_tinycup_$timestamp.zip" *
+sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/* -X "export/nateserk_tinycup.zip" *
 sudo rm ".DS_Store"
