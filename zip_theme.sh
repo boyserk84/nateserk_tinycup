3a450f3190a22e9d5133f616cee77af3350fb90f
Nate Kem <me@nateserk.com>
2017-03-20 21:51:31 -0700

    [ Shell Script ] Work in progress. Adding an ability to rename the theme via shell script.

--- zip_theme.sh
+++ zip_theme.sh
@@ -2,5 +2,21 @@
 #!/bin/bash
 timestamp=$(date +%s)
 echo "Zipping 'nateserk_tinycup' theme generated at timestamp=$timestamp \n";
-sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/* -X "export/nateserk_tinycup.zip" *
-sudo rm ".DS_Store"
+
+if [ "$1" != "" ]; then
+  if [ "$1" == "--rename" ]; then
+    if [ "$2" != "" ]; then
+      grep -rl 'nateserk_tinycup' ./ | xargs sed -i 's/nateserk_tinycup/$2/g'
+    else
+      echo "Invalid --rename parameter. Abort!"
+      exit
+    fi
+  else
+    echo "Invalid command. Abort!"
+    exit
+  fi
+else
+  echo "No argument suppled. Archiving 'nateserk_tinycup.zip"
+fi
+#sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/* -X "export/nateserk_tinycup.zip" *
+#//sudo rm ".DS_Store"
