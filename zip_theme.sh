611e90772d02a9ecc7056b89248a4d8e55424c83
Nate Kem <me@nateserk.com>
2017-03-20 22:00:18 -0700

    [ Shell Script ] Commented out --rename option part. Working in progress.

--- zip_theme.sh
+++ zip_theme.sh
@@ -3,20 +3,23 @@
 timestamp=$(date +%s)
 echo "Zipping 'nateserk_tinycup' theme generated at timestamp=$timestamp \n";
 
-if [ "$1" != "" ]; then
-  if [ "$1" == "--rename" ]; then
-    if [ "$2" != "" ]; then
-      grep -rl 'nateserk_tinycup' ./ | xargs sed -i 's/nateserk_tinycup/$2/g'
-    else
-      echo "Invalid --rename parameter. Abort!"
-      exit
-    fi
-  else
-    echo "Invalid command. Abort!"
-    exit
-  fi
-else
-  echo "No argument suppled. Archiving 'nateserk_tinycup.zip"
-fi
-#sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/* -X "export/nateserk_tinycup.zip" *
-#//sudo rm ".DS_Store"
+# if [ "$1" != "" ]; then
+#   if [ "$1" == "--rename" ]; then
+#     if [ "$2" != "" ]; then
+#       echo "Renaming 'nateserk_tinycup' to $2"
+#       # TODO: Work in progress
+#       grep -rl --exclude-dir="./git" 'nateserk_tinycup' ./ | xargs sed -i 's/nateserk_tinycup/$2/g'
+#     else
+#       echo "Invalid --rename parameter. Abort!"
+#       exit
+#     fi
+#   else
+#     echo "Invalid command. Abort!"
+#     exit
+#   fi
+# else
+#   echo "No argument suppled. Archiving 'nateserk_tinycup.zip"
+# fi
+
+sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/* -X "export/nateserk_tinycup.zip" *
+sudo rm ".DS_Store"
