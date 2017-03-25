9cf54867e566518101f9a3b9daa4d015490c53e0
Nate Kem <me@nateserk.com>
2017-03-25 10:13:32 -0700

    [ Shell Script ] Update zip_theme.sh script for rename options.

--- zip_theme.sh
+++ zip_theme.sh
@@ -3,23 +3,31 @@
 timestamp=$(date +%s)
 echo "Zipping 'nateserk_tinycup' theme generated at timestamp=$timestamp \n";
 
-# if [ "$1" != "" ]; then
-#   if [ "$1" == "--rename" ]; then
-#     if [ "$2" != "" ]; then
-#       echo "Renaming 'nateserk_tinycup' to $2"
-#       # TODO: Work in progress
-#       grep -rl --exclude-dir=".git" --exclude="zip_theme.sh" --exclude-dir="./export" 'nateserk_tinycup' . | xargs sed -i 's/nateserk_tinycup/$2/g'
-#     else
-#       echo "Invalid --rename parameter. Abort!"
-#       exit
-#     fi
-#   else
-#     echo "Invalid command. Abort!"
-#     exit
-#   fi
-# else
-#   echo "No argument suppled. Archiving 'nateserk_tinycup.zip"
-# fi
+if [ "$1" != "" ]; then
+  if [ "$1" == "--rename" ]; then
+    if [ "$2" != "" ]; then
+      echo "[ Running ] Renaming 'nateserk_tinycup' to $2"
+      # TODO: Work in progress
+      if [[ "$OSTYPE" == "darwin"* ]];
+        echo "[ Running ] Detecting 'macOSX'. Update 'rename' options. "
+        # if running on mac OSX, we need -i '' and -e options.
+        # Reference: http://stackoverflow.com/questions/19456518/invalid-command-code-despite-escaping-periods-using-sed
+        grep -rl --exclude-dir=".git" --exclude="zip_theme.sh" --exclude-dir="./export" 'nateserk_tinycup' . | xargs sed -i '' -e 's/nateserk_tinycup/'"$2"'/g'
+      else
+        grep -rl --exclude-dir=".git" --exclude="zip_theme.sh" --exclude-dir="./export" 'nateserk_tinycup' . | xargs sed -i 's/nateserk_tinycup/'"$2"'/g'
+      fi
+    else
+      echo "[ Error ] Invalid --rename parameter. Abort!"
+      exit
+    fi
+  else
+    echo "[ Error ] Invalid command. Abort!"
+    exit
+  fi
+else
+  echo "[ Warning ] No argument supplied. Archiving 'nateserk_tinycup.zip"
+fi
 
-sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/* -X "export/nateserk_tinycup.zip" *
+echo "[ Running ] Archiving theme to '"$2"'.zip "
+sudo zip -r --exclude=zip_theme.sh --exclude=*.DS_Store* --exclude=export/* -X "export/$2.zip" *
 sudo rm ".DS_Store"
