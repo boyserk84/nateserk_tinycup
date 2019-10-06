ffcaf0841fc7466ebc45176e517a6415f72efd22
Nate Kem <me@nateserk.com>
2019-10-06 16:10:07 -0700

    [ ShortCode Feature ] Custom button with link.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -48,3 +48,39 @@ if ( ! function_exists( 'nateserk_tinycup_open_empty_modal_dialog' ) ) :
   }
 endif;
 add_shortcode('open_modal_dialog', 'nateserk_tinycup_open_empty_modal_dialog');
+
+/**
+* [custom_link_button button_track_id="id" button_url="http://www.blahblahb.com" button_class="btn-danger" button_icon="fa-camera-retro fa-4x"] Your Text [custom_link_button]
+* Short Code: Create a customized button.
+*/
+if ( ! function_exists( 'nateserk_tinycup_custom_link_button' ) ) :
+
+  function nateserk_tinycup_custom_link_button( $atts, $content=null ) {
+      $a = shortcode_atts(
+      array(
+          'button_url'=> "",
+          'button_class'=> "btn-primary",
+          'button_icon'=> "",
+          'button_track_id' => ""
+      ), $atts);
+
+      $uniqueId = $a['button_track_id'];
+      $linkUrl = $a['button_url'];
+      $iconHtml = "";
+      if ( !empty( $a['button_icon'] ) ) {
+        $iconHtml = '<i class="fa ' .$a['button_icon'] .'" aria-hidden="true"></i> ';
+      }
+
+      return "<a href=\"$linkUrl\"><button type=\"button\" class=\"btn " .$a['button_class'] ." btn-lg\">"
+      .$iconHtml .$content
+        ."</button></a><style type=\"text/css\">
+            body.modal-open #main,
+            body.modal-open #masthead,
+            body.modal-open .widget-area,
+            body.modal-open .site-footer{
+              -webkit-filter:none;
+            }
+          }</style>";
+  }
+endif;
+add_shortcode('custom_link_button', 'nateserk_tinycup_custom_link_button');
