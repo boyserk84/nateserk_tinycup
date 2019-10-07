7060896a35af1f3c50dcedf50645c4f238aea3a4
Nate Kem <me@nateserk.com>
2019-10-06 17:23:06 -0700

    [ ShortCode Feature ] Update documentation for 'custom_button_link' shortcode.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -8,7 +8,7 @@
  */
 
 /**
-* [open_modal_dialog button_text="Click Here" button_class="btn-danger" button_icon="fa-camera-retro fa-4x"] Your Content [open_modal_dialog]
+* [open_modal_dialog button_text="Click Here" button_class="btn-danger" button_icon="fa-camera-retro fa-4x"] Your Content [/open_modal_dialog]
 * Short Code: Create a button for trigger an empty modal dialog.
 */
 if ( ! function_exists( 'nateserk_tinycup_open_empty_modal_dialog' ) ) :
@@ -50,7 +50,7 @@ endif;
 add_shortcode('open_modal_dialog', 'nateserk_tinycup_open_empty_modal_dialog');
 
 /**
-* [custom_link_button button_track_id="id" button_url="http://www.blahblahb.com" button_class="btn-danger" button_icon="fa-camera-retro fa-4x"] Your Text [custom_link_button]
+* [custom_link_button button_track_id="id" button_alt_text="Link to this" button_url="http://www.blahblahb.com" button_class="btn-danger" button_icon="fa-camera-retro fa-4x"] Your Text [/custom_link_button]
 * Short Code: Create a customized button.
 */
 if ( ! function_exists( 'nateserk_tinycup_custom_link_button' ) ) :
@@ -61,26 +61,30 @@ if ( ! function_exists( 'nateserk_tinycup_custom_link_button' ) ) :
           'button_url'=> "",
           'button_class'=> "btn-primary",
           'button_icon'=> "",
-          'button_track_id' => ""
+          'button_track_id' => "",
+          'button_alt_text' => ""
       ), $atts);
 
       $uniqueId = $a['button_track_id'];
       $linkUrl = $a['button_url'];
+      $altText = ( !empty($a['button_alt_text']) )? $a['button_alt_text'] : ("Click to visit ".$linkUrl);
       $iconHtml = "";
       if ( !empty( $a['button_icon'] ) ) {
         $iconHtml = '<i class="fa ' .$a['button_icon'] .'" aria-hidden="true"></i> ';
       }
 
-      return "<a href=\"$linkUrl\"><button type=\"button\" class=\"btn " .$a['button_class'] ." btn-lg\">"
-      .$iconHtml .$content
-        ."</button></a><style type=\"text/css\">
-            body.modal-open #main,
-            body.modal-open #masthead,
-            body.modal-open .widget-area,
-            body.modal-open .site-footer{
-              -webkit-filter:none;
-            }
-          }</style>";
+      $htmlButton = "<button type=\"button\" class=\"btn " .$a['button_class'] ." btn-lg\">"
+        .$iconHtml .$content ."</button>";
+
+      
+      if ( !empty($a['button_url']) ) {
+        return "<a href=\"$linkUrl\" alt=\"$altText\">$htmlButton</a>";
+      } else {
+        return $htmlButton;
+      }
+
+
+
   }
 endif;
 add_shortcode('custom_link_button', 'nateserk_tinycup_custom_link_button');
